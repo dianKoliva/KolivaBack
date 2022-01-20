68a6f237542abd8207da72abc6ac314bdb4b3f4a
dianKoliva <dianpretty01@gmail.com>
2022-01-20 21:08:01 +0200

    new changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/userModel');
 
-const userControl=require("../controllers/user.js")
+const userControl=require("../controllers/userControllers.js")
 router.post('/signup',userControl.signUp )
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express();
 const bodyParser = require("body-parser");
 require('dotenv').config();
+var users = require("./routes/userRoutes.js");
 require("./db/connect")
 
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-
+app.use('/km/users', users);
 
 app.listen(()=>{
     console.log(`Listening on port ${process.env.port}`);

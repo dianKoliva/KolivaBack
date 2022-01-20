f8468f18e8a1a84c9e73062de692d142f3315538
dianKoliva <dianpretty01@gmail.com>
2022-01-20 21:22:55 +0200

    new changes

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 require('dotenv').config();
 var users = require("./routes/userRoutes.js");
 require("./db/connect")
+const morgan =require( "morgan")
 
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(morgan('dev'))
 app.use(bodyParser.json());
 app.use('/km/users', users);
 

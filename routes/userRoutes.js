4ff86aa6d75665f6c07def71a5e130cd036573ba
dianKoliva <dianpretty01@gmail.com>
2022-01-20 21:04:07 +0200

    new changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/userModel');
 
+const userControl=require("../controllers/user.js")
+router.post('/signup',userControl.signUp )
 
-
+module.exports = router;

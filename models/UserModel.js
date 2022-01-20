f764baf03b9ab891950ce97bed1dfa5e5289593d
dianKoliva <dianpretty01@gmail.com>
2022-01-20 20:51:52 +0200

    new changes

---
+++ models/UserModel.js
@@ -0,0 +1,20 @@
+var mongoose=require('mongoose')
+var Schema =mongoose.Schema;
+
+var User=new Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    email: { type: String,
+         required: true,
+          unique:true,
+        match:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ },
+    password: { type: String, required: true },
+    status: { 
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active'
+     }
+});
+
+var User = mongoose.model('User', User);
+module.exports = User;

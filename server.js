5697acf99fe3fcc2fd320234f8e0bb0dce3685c3
dianKoliva <dianpretty01@gmail.com>
2022-01-20 20:15:01 +0200

    new changes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express=require('express')
 const app=express();
+const bodyParser = require("body-parser");
+require('dotenv').config();
 const port=400;
 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 app.listen(port,()=>{

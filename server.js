96a80ba7fa4672a429739261c3e544094c3476dc
dianKoliva <dianpretty01@gmail.com>
2022-01-21 08:37:55 +0200

    new changes

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 app.use('/km/users', users);
 app.use('/km/abakozi',abakozi);
 
-
+var date=new Date()
 
 
 app.listen(process.env.port,()=>{
     console.log(`Listening on port ${process.env.port}`);
-
+    console.log( date.toString().split("T"))
 })

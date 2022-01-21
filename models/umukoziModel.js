40d869574355051a6387ab9a6bafbd2e6412d4ad
dianKoliva <dianpretty01@gmail.com>
2022-01-21 07:33:42 +0200

    new changes

---
+++ models/umukoziModel.js
@@ -0,0 +1,17 @@
+var mongoose=require('mongoose')
+var Schema =mongoose.Schema;
+
+var Umukozi=new Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    amazina: { type: String, required: true },
+    nimero:{type:Number,required:true, unique:true,},
+    umushahara:{type:Number,required:true},
+    created_at:{type:Date,required:true},
+    deactivation_date:{type:Date,required:false},
+    status: { 
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active'
+     }
+
+})
